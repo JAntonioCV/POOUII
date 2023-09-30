f91f5ef5eb53cdbc25e77e212cf2d8fa16f151f9
Josiel Castillo <josielcastillov@gmail.com>
2023-09-30 15:07:09 -0600

    Agregar archivos de proyecto.

---
+++ POOUII1/Persona.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOUII1
+{
+    //Clase Persona
+    internal class Persona
+    {
+        //Campos estaticos
+        public static int ContadorPersona = 0;
+        public static readonly double PI = 3.1416;
+
+        //Atributos o propiedades
+        private string _Cedula;
+        private string _Nombres;
+        private string _Apellidos;
+        private int _Edad;
+        private double _Peso;
+
+        //Metodos getter y setter que sirven para leer y escribir propiedades privadas
+        public double Peso
+        {
+            get { return _Peso; }
+            set { _Peso = value; }
+        }
+
+        public int Edad
+        {
+            get { return _Edad; }
+            set { _Edad = value; }
+        }
+        public string Apellidos
+        {
+            get { return _Apellidos; }
+            set { _Apellidos = value; }
+        }
+
+        public string Nombres
+        {
+            get { return _Nombres; }
+            set { _Nombres = value; }
+        }
+
+        public string Cedula
+        {
+            get { return _Cedula; }
+            set { _Cedula = value; }
+        }
+
+        //Propiedades Autoimplementadas
+        public bool EsEstudiante { get; set; }
+        public char Genero { get; set; }
+
+        //Constructor por Defecto
+        public Persona()
+        {
+            ContadorPersona++;
+        }
+
+        //Sobrecarga de metodos construir
+        public Persona(string cedula, string nombres, string apellidos, int edad, double peso, bool EsEstudiante, char genero)
+        {
+
+            _Cedula = cedula;
+            _Nombres = nombres;
+            _Apellidos = apellidos;
+            _Edad = edad;
+            _Peso = peso;
+            this.EsEstudiante = EsEstudiante;
+            Genero = genero;
+            ContadorPersona++;
+        }
+
+        //Metodos 
+        public void Saludar()
+        {
+            Console.WriteLine("Hola Mundo " + _Nombres);
+        }
+
+        //Metodos sobreCargados
+        public void Saludar(string nombre)
+        {
+            Console.WriteLine("Hola Mundo {0} yo soy {1} persona", nombre, _Nombres);
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Cedula: {_Cedula}, Nombres: {_Nombres}, Edad: {_Edad}, Peso: {_Peso}, Estudiante: {EsEstudiante}, Genero: {Genero}");
+        }
+
+        //Metodos Estaticos
+        public static void MostrarPI()
+        {
+            Console.WriteLine("El valor de PI es: " + PI);
+        }
+
+        public static void CantidadDePersonas()
+        {
+            Console.WriteLine("La cantidad de persona es: {0}", ContadorPersona);
+        }
+
+
+
+        }
+
+    }
+
+    
---
+++ POOUII1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOUII1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("La Cantidad de Personas Instaciadas: " + Persona.ContadorPersona);
+            //Console.WriteLine("El valor de PI es: " + Persona.PI);
+            //Persona.ContadorPersona = 100;
+            //Console.WriteLine("La Cantidad de Personas Instaciadas: " + Persona.ContadorPersona);
+
+            Persona persona1 = new Persona();
+
+            persona1.Cedula = "001-100799-0000V";
+            persona1.Nombres = "Pedro Pablo";
+            persona1.Apellidos = "Martinez Gutierrez";
+            persona1.Edad = 24;
+            persona1.Peso = 165.8;
+            persona1.EsEstudiante = false;
+            persona1.Genero = 'M';
+
+            //Console.WriteLine("La Cantidad de Personas Instaciadas: " + Persona.ContadorPersona);
+
+            Persona persona2 = new Persona("570-100798-11124Q", "Juana Maria", "Perez Zeledon", 50, 120.5, true, 'F');
+
+            //Console.WriteLine("Persona 2 Agregada");
+
+            //Console.WriteLine("La Cantidad de Personas Instaciadas: " + Persona.ContadorPersona);
+
+            //persona1.Saludar();
+            //persona2.Saludar();
+
+            //persona1.Saludar("Pepito");
+            //persona2.Saludar("Violeta");
+
+            //persona1.MostrarInformacion();
+            //persona2.MostrarInformacion();
+
+            Persona.MostrarPI();
+            Persona.CantidadDePersonas();
+
+
+            Console.ReadLine();
+        }
+    }
+}
